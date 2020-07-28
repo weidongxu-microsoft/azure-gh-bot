a25646eeb2150130e57c411ed1f290ee31553c74
Weidong Xu <weidxu@microsoft.com>
2020-07-28 19:28:01 +0800

    integrate text analytics

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { request } from '@octokit/request'
+import * as url from 'url'
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on('issues', async (context) => {
+    if (context.payload.action == 'opened') {
+      const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+      await context.github.issues.createComment(issueComment)
+    }
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    if (context.payload.action == 'opened' || (context.payload.action == 'edited')) {
+      const labels: string[] = []
+      if (context.payload.issue.title.toLowerCase().includes('[feature request]')) {
+        labels.push('feature-request')
+      } else if (context.payload.issue.title.toLowerCase().includes('[bug]')) {
+        labels.push('bug')
+      }
+
+      if (context.payload.issue.body.length > 100 && context.payload.issue.body.length < 5120) {
+        const textAnalyticsKey = process.env['TEXT_ANALYTICS_KEY'] || ''
+        const textAnalyticsEndPoint = process.env['TEXT_ANALYTICS_ENDPOINT'] || ''
+
+        const response = await request('POST ' + url.resolve(textAnalyticsEndPoint, '/text/analytics/v2.0/keyPhrases'), {
+          headers: {
+            'Ocp-Apim-Subscription-Key': textAnalyticsKey,
+          },
+          mediaType: {
+            format: 'json'
+          },
+          documents: [
+            {
+              'language': 'en',
+              'id': Date.now().toString(),
+              'text': context.payload.issue.body
+            }
+          ]
+        })
+        app.log(response)
+      }
+
+      if (labels.length > 0) {
+        const issueAddLabels = context.issue({ labels: labels })
+        await context.github.issues.addLabels(issueAddLabels)
+      }
+    }
+  })
 }
